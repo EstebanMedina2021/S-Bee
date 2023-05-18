86f5c49d352dc1624cf41dcde8f11a7d3ab42c22
luouy21785 <133832221+luouy21785@users.noreply.github.com>
2023-05-18 11:22:55 +0800

    Update test.py

--- modles/test.py
+++ modles/test.py
@@ -13,7 +13,7 @@ from FCN import FCN8s, FCN16s, FCN32s, FCNs, VGGNet
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
-model = torch.load('checkpoints/fcn_model.pt')  # 加载模型
+model = torch.load('checkpoints/fcn_model.pt') 
 model = model.to(device)
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -21,12 +21,12 @@ transform = transforms.Compose([
 
 def get_img_list(dir, firelist, ext=None):
     newdir = dir
-    if os.path.isfile(dir):  # 如果是文件
+    if os.path.isfile(dir): 
         if ext is None:
             firelist.append(dir)
         elif ext in dir[-3:]:
             firelist.append(dir)
-    elif os.path.isdir(dir):  # 如果是目录
+    elif os.path.isdir(dir):  
         for s in os.listdir(dir):
             newdir = os.path.join(dir, s)
             get_img_list(newdir, firelist, ext)
@@ -39,7 +39,7 @@ def read_img():
     imgall = []
     for imgpath in imglist:
         # print(imgpath)
-        imgname = os.path.split(imgpath)[1]  # 分离文件路径和文件名后获取文件名（包括了后缀名）
+        imgname = os.path.split(imgpath)[1] 
         print(imgname)
         img = cv2.imread(imgpath, cv2.IMREAD_COLOR)
         sift(img, imgname)
@@ -55,7 +55,7 @@ def sift(img,i):
     # img = cv2.imread('./Snapshot020.jpg')
     # img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     if __name__ == '__main__':
-        # img_name = r'./test/0.jpg'  # 预测的图片
+        # img_name = r'./test/0.jpg'  
         # imgA = cv2.imread(img_name)
         # print(imgA.shape)
         # x, y = imgA.shape[0:2]
